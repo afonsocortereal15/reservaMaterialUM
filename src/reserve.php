76e3b7483b128571a343c0d82629f822d42d4364
Afonso Corte-Real <afonsocortereal15@gmail.com>
2024-05-20 15:53:52 +0100

    Add comments to the code

--- src/reserve.php
+++ src/reserve.php
@@ -1,19 +1,30 @@
 <?php
+// Include the necessary connection file
 include("../inc/connect.inc");
 
-  global $conn;
-  $typeMaterial = $_POST['materialType'];
-  $material = $_POST['material'];
-  $startReservation = str_replace("T", " ", $_POST['startReservation']);
-  $endReservation = str_replace("T", " ", $_POST['endReservation']);
+// Global database connection variable
+global $conn;
 
-  $sql = "INSERT INTO reservations VALUES (NULL, '$startReservation', '$endReservation', '1', '2')";
+// Convert the start and end reservation times from ISO 8601 format to MySQL datetime format
+$startReservation = str_replace("T", " ", $_POST['startReservation']);
+$endReservation = str_replace("T", " ", $_POST['endReservation']);
 
-  if (mysqli_query($conn, $sql)) {
-    $sql2 = "UPDATE materials SET statusMaterial = 1 WHERE materials.idMaterial = $material";
-    mysqli_query($conn, $sql2);
+// Get the user and material information from the POST request
+$user = $_POST['user'];
+$material = $_POST['material'];
 
-    header("Location: ../public/searchResult.php");
-  } else {
-    echo "Error: " . $sql2 . "<br>" . mysqli_error($conn);
-  }
+// Prepare the SQL query to insert a new reservation into the database
+$sql = "INSERT INTO reservations VALUES (NULL, '$startReservation', '$endReservation', '$user', '$material')";
+
+// Execute the SQL query and check for any errors
+if (mysqli_query($conn, $sql)) {
+  // If the reservation was successfully added, update the material status to 'reserved'
+  $sql2 = "UPDATE materials SET statusMaterial = 1 WHERE materials.idMaterial = $material";
+  mysqli_query($conn, $sql2);
+
+  // Redirect the user to the search result page
+  header("Location:../public/searchResult.php");
+} else {
+  // If there was an error, display the error message
+  echo "Error: ". $sql. "<br>". mysqli_error($conn);
+}
